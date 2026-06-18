77c1e7cb7614e4707f2db6eec84b775f273ded18
GugulothBhuvan <bhuvanrajnaik@gmail.com>
2026-06-18 16:00:07 +0530

    fix: use statically compiled musl binary for Tectonic

--- backend/build.sh
+++ backend/build.sh
@@ -7,8 +7,8 @@ pip install -r requirements.txt
 
 # Download and install Tectonic for Linux (Render)
 echo "Downloading Tectonic for Linux..."
-wget https://github.com/tectonic-typesetting/tectonic/releases/download/tectonic%400.14.1/tectonic-0.14.1-x86_64-unknown-linux-gnu.tar.gz
-tar -xzf tectonic-0.14.1-x86_64-unknown-linux-gnu.tar.gz
+wget https://github.com/tectonic-typesetting/tectonic/releases/download/tectonic%400.14.1/tectonic-0.14.1-x86_64-unknown-linux-musl.tar.gz
+tar -xzf tectonic-0.14.1-x86_64-unknown-linux-musl.tar.gz
 mkdir -p bin
 mv tectonic bin/tectonic
 chmod +x bin/tectonic
